2671c394b3f166de6dcf6d7ca02058f387753f3c
edlectrico <edlectrico@gmail.com>
2015-08-20 13:13:29 +0200

    Adding more examples

--- character_recognition.py
+++ character_recognition.py
@@ -5,12 +5,14 @@ from sklearn import svm
 # digits contains images, data, target...
 # http://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_digits.html
 digits = datasets.load_digits()
-# print digits.data.shape	# (1797, 64)
+# print digits.data.shape	# (1797, 64) 1797 examples of digit data in 8x8 matrices
+# print len(digits.data)	# 1797
 
 # Visualizing some sample data
 '''
 plt.gray()
 plt.matshow(digits.images[0]) # prints the first image (obviously, its zero)
+print digits.data[0]
 plt.show()
 '''
 '''
@@ -22,8 +24,13 @@ print digits.target	# the label we assign to the digits
 # http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
 classifier = svm.SVC(gamma=0.001) # The gamma parameter is optional
 
-data = digits.data[:-10] 	# all but the last 10 to train, the last 10 to test
+data = digits.data[:-1] # from 1797 we pick all but the last element to train, the last one to test
+target = digits.target[:-1]
+'''
+data = digits.data[:-10]
 target = digits.target[:-10]
+'''
+
 '''
 print data
 print target
@@ -31,9 +38,15 @@ print target
 # Training the classifier
 classifier.fit(data, target)
 
-# Predicting the 5th from the last element
-print 'Prediction: ' + str(classifier.predict(digits.data[-5]))
+# Predicting the 1st from the last element (the negative first element)
+print 'Prediction: ' + str(classifier.predict(digits.data[-1]))
+'''
+print 'Prediction: ' + str(classifier.predict(digits.data[-2]))
+'''
 
 # Visualization with matplotlib
-plt.imshow(digits.images[-5], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.imshow(digits.images[-1], cmap=plt.cm.gray_r, interpolation='nearest')
+'''
+plt.imshow(digits.images[-2], cmap=plt.cm.gray_r, interpolation='nearest')
+'''
 plt.show()
