b3b737cdb0b41d5de6c39d2938a8c1082159fe43
edlectrico <edlectrico@gmail.com>
2015-08-24 12:08:45 +0200

    Refactoring

--- data_acquisition.py
+++ data_acquisition.py
@@ -4,7 +4,12 @@ import os
 import time # filenames are dates
 from datetime import datetime
 
-path = "/home/edlectrico/Downloads/intraQuarter" 	# the unziped folder path
+data_dir = "/home/edlectrico/Downloads/"
+path = data_dir + "intraQuarter" 			# data folder
+split_before_value = ':</td><td class="yfnc_tabledata1">'
+split_after_value = '</td>'
+split_before_stock = '</small><big><b>'
+split_after_stock = '</b></big>'
 
 # Collecting Debt/Equity
 def Key_Stats(gather="Total Debt/Equity (mrq)"):	# In the data from each website there
@@ -12,25 +17,34 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):	# In the data from each website
   stock_list = [x[0] for x in os.walk(statspath)]	# for each file in path...
   df = pd.DataFrame(columns = ['Date','Unix','Ticker','DE Ratio'])
   
+  sp500_df = pd.DataFrame.from_csv(data_dir + "YAHOO-INDEX_GSPC.csv")
+  
   for each_dir in stock_list[1:]: 			# stock_list[0] points at the root directory...
     each_file = os.listdir(each_dir)
-    # print 'each_dir: ' + str(each_dir)
-    # print 'each_file: ' + str(each_file)
-    ticker = each_dir.split(path + '/_KeyStats/')[1] # home/edlectrico/Downloads/intraQuarter/_KeyStats/
-    print 'ticker: ' + str(ticker)
+    ticker = each_dir.split(path + '/_KeyStats/')[1] 	# home/edlectrico/Downloads/intraQuarter/_KeyStats/
+    
     if len(each_file) > 0:
       for file in each_file:
         date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html') # The format of the file's name
         unix_time = time.mktime(date_stamp.timetuple())
-        # print date_stamp, unix_time
         file_path = each_dir + '/' + file
-	source = open(file_path, 'r').read()	# Reading the full html file to extract the desired value
-	print file_path
+	source = open(file_path, 'r').read() 		# Reading each html file to extract the desired value
+
 	try:
-	  value = source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0] # Split by 'gather' term
+	  value = source.split(gather + split_before_value)[1].split(split_after_value)[0] # Split by 'gather' term	  
+	  try:
+	    sp500_date = datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d')
+	    row = sp500_df[(sp500_df.index == sp500_date)]
+	    sp500_value = float(row["Adjusted Close"])
+	  except:
+            sp500_date = datetime.fromtimestamp(unix_time-259200).strftime('%Y-%m-%d') # 3 days in seconds (avoid weekends)
+            row = sp500_df[(sp500_df.index == sp500_date)]
+            sp500_value = float(row["Adjusted Close"]) 
+
+	  stock_price = float(source.split(split_before_stock)[1].split(split_after_stock)[0])
+
 	  # Finally assign the values to the DataFrame columns
 	  df = df.append({'Date':date_stamp,'Unix':unix_time,'Ticker':ticker,'DE Ratio':value,}, ignore_index = True)
-	  print ticker + ":" + str(value)
 	except IndexError:
 	  print "Can't get value from %s" %file_path
 
